4ec4ce34661fab890aa3f7690d1fbeecbf717249
Philipp Hug <philipp@hug.cx>
2011-07-28 15:29:27 +0200

    Imported Upstream version 8.3.9

---
+++ drbd/drbd_buildtag.c
@@ -0,0 +1,7 @@
+/* automatically generated. DO NOT EDIT. */
+#include <linux/drbd.h>
+const char *drbd_buildtag(void)
+{
+	return "GIT-hash: 1c3b2f71137171c1236b497969734da43b5bec90"
+		" build by phil@fat-tyre, 2010-10-22 17:09:03";
+}
